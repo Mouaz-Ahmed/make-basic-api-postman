e2d27d55f82243a5c5223e835bcf5e5faf35a51e
Mouaz-Ahmed <mouazahmed321@gmail.com>
2023-02-08 22:14:11 +0500

    initial

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'mouaz', email: 'mouaz@gmail.com' , contact: '+92///' },
+    { name: 'ahmed', email: 'ahmed@gmail.com' , contact: '+92///' },
+    { name: 'khan', email: 'khan@gmail.com' , contact: '+92///' }
+]
+module.exports = data;
